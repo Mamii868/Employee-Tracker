cead661e4705f64379701442ae3afa04a1c5dcf2
Marc Moten <marcmotenii@gmail.com>
2021-10-02 16:46:14 -0500

    edited tables

--- db/schema.sql
+++ db/schema.sql
@@ -4,15 +4,17 @@ CREATE DATABASE departments_db;
 USE departments_db;
 
 CREATE TABLE department(
-    id INT NOT NULL PRIMARY KEY,
+    id INT NOT NULL AUTO_INCREMENT,
     name VARCHAR(30) NOT NULL,
+    PRIMARY KEY (id)
 );
 
 CREATE TABLE role(
-    id INT NOT NULL PRIMARY KEY,
+    id INT NOT NULL AUTO_INCREMENT,
     title VARCHAR(30) NOT NULL,
     salary DECIMAL NOT NULL,
     department_id INT,
+     PRIMARY KEY (id)
     FOREIGN KEY (department_id) REFERENCES department(id) 
 );
 
