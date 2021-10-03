86b1a146cb74688cb4bc98d5223cd9c725403997
Marc Moten <marcmotenii@gmail.com>
2021-10-03 14:15:07 -0500

    changed logs to tables, added tables to view when moving to add menus

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const inquirer = require('inquirer')
+const mysql = require('mysql2')
 
 const db = mysql.createConnection(
     {
@@ -38,6 +39,14 @@ const addMenu = [
         choices: ['Add a Department', 'Add a Role', 'Add an Employee', 'Update an Employee Role', 'Back']
     }
 ];
+
+const addDepartmentPrompt = [
+    {
+        type: 'input',
+        message: 'Please enter new department name',
+        name: 'name'
+    }
+]
 
  function mainMenu(){ 
     inquirer.prompt(startMenu)
@@ -93,15 +102,19 @@ function changeViewMenu(answers) {
 function changeAddMenu(answers) {
     switch (answers.add) {
         case 'Add a Department':
-            addDepartment()
+            showAllDepartments();
+            addDepartmentMenu();
             break;
         case 'Add a Role':
-            addRole()
+            showAllRoles();
+            addRole();
             break;
         case 'Add an Employee':
+            showAllEmployees();
             addEmployee();
             break;
         case 'Update An Employee Role':
+            showAllEmployees();
             updateEmployeeRole();
             break;
         case 'Back':
@@ -110,16 +123,53 @@ function changeAddMenu(answers) {
     }
 };
 
+function addDepartmentMenu() {
+    inquirer.prompt(addDepartmentPrompt)
+    .then((answers) => {
+        addDepartment(answers)
+    })
+}
+
+
 function showAllEmployees() {
     db.query('SELECT * FROM employee', function (err, results) {
-        console.log(results);
+        console.table(results);
+        mainMenu();
     })  
   };
 
 function showAllDepartments() {
     db.query('SELECT * FROM department', function (err, results) {
-        console.log(results);
+        console.table(results);
+        mainMenu();
+    })  
+}
+// Takes answers and adds rows to tables
+function addDepartment(answers) {
+    db.query(`INSERT INTO department (name) VALUES ('${answers.name}')`, function (err, results) {
+        console.log("added " + results.affectedRows + " Department/s")
+        showAllDepartments();
+    })
+    
+}
+
+function addEmployee(answers) {
+
+}
+
+function addRole(answers) {}
+
+//Used to display data without redirecting to the Main Menu
+function departmentList() {
+    db.query('SELECT * FROM department', function (err, results) {
+        console.table(results);
+    })  
+}
+function employeeList() {
+    db.query('SELECT * FROM employee', function (err, results) {
+        console.table(results);
     })  
 }
 
+//Starts program
  mainMenu();
