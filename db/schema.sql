5ec0bce7b8314270758ee7661e8d3639333d6e63
Marc Moten <marcmotenii@gmail.com>
2021-10-04 12:40:11 -0500

    fixed schema bug

--- db/schema.sql
+++ db/schema.sql
@@ -1,29 +1,29 @@
 DROP DATABASE IF EXISTS departments_db;
 CREATE DATABASE departments_db;
-
 USE departments_db;
-
-CREATE TABLE department(
-    id INT NOT NULL AUTO_INCREMENT,
-    name VARCHAR(30) NOT NULL,
-    PRIMARY KEY (id)
+DROP TABLE IF EXISTS department;
+DROP TABLE IF EXISTS role;
+DROP TABLE IF EXISTS employee;
+CREATE TABLE department (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(30) NOT NULL
 );
-
-CREATE TABLE role(
-    id INT NOT NULL AUTO_INCREMENT,
+CREATE TABLE role (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30) NOT NULL,
-    salary DECIMAL NOT NULL,
+    salary INT,
     department_id INT,
-     PRIMARY KEY (id)
-    FOREIGN KEY (department_id) REFERENCES department(id) 
+    FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE
+    SET NULL
 );
-
-CREATE TABLE employee(
+CREATE TABLE employee (
     id INT NOT NULL PRIMARY KEY,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
-    role_id INT NOT NULL,
+    role_id INT,
     manager_id INT,
-    FOREIGN KEY (manager_id) REFERENCES employee(id),
-    FOREIGN KEY (role_id) REFERENCES role(id)
+    FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE
+    SET NULL,
+        FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE
+    SET NULL
 );
