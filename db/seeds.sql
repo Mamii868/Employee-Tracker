8ed5671ff7d8ba8a24ae1c6fe312fa515f862d3e
Marc Moten <marcmotenii@gmail.com>
2021-10-04 12:40:23 -0500

    added more seed data

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,6 +1,11 @@
--- INSERT INTO employee (id, first_name, last_name)
--- VALUES (1, "Marc", "Moten" ),
--- (2, "Michael", "Moten");
 
-INSERT INTO department (id, name) 
-VALUES (1, "Head")
+
+INSERT INTO department (id, name)
+VALUES (1, "Hwad");
+
+INSERT INTO role (id, title, salary, department_id)
+VALUES (1, "Boss", 250000, 1);
+
+INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
+VALUES (1, "Marc", "Moten", 1, NULL),
+(2, "Michael", "Moten", 1, 1);
