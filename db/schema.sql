cdfcd5ada4fd05ce847a7506d05d3158403ce21b
Marc Moten <marcmotenii@gmail.com>
2021-10-09 01:33:41 -0500

    updated schema

--- db/schema.sql
+++ db/schema.sql
@@ -11,13 +11,13 @@ CREATE TABLE department (
 CREATE TABLE role (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30) NOT NULL,
-    salary INT,
+    salary DECIMAL,
     department_id INT,
     FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE
     SET NULL
 );
 CREATE TABLE employee (
-    id INT NOT NULL PRIMARY KEY,
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
     role_id INT,
